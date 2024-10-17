6317be0ee92047fa3eec99c35089e3639f5157f2
rya23 <thakur.aarya23@gmail.com>
2024-10-18 00:48:27 +0530

    Bfs added

---
+++ ai/bfs_uninformed.py
@@ -0,0 +1,41 @@
+from collections import defaultdict
+
+
+class Graph:
+
+    def __init__(self):
+
+        self.graph = defaultdict(list)
+
+    def addEdge(self, u, v):
+
+        self.graph[u].append(v)
+
+    def BFS(self, source):
+        visited = [False] * (max(self.graph) + 1)
+
+        queue = []
+        queue.append(source)
+        visited[source] = True
+        while queue:
+
+            s = queue.pop(0)
+            print(s, end=" -> ")
+
+            for i in self.graph[s]:
+                if not visited[i]:
+                    queue.append(i)
+                    visited[i] = True
+
+
+g = Graph()
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 2)
+g.addEdge(2, 0)
+g.addEdge(2, 3)
+g.addEdge(3, 3)
+
+
+print("Starting BFS")
+g.BFS(0)
---
+++ ai/readme.md
@@ -0,0 +1,8 @@
+# AI Lab Code
+- Implement any one of the Uninformed search techniques
+
+- Implement any one of the Informed search techniques
+
+- Implement adversarial search using min-max algorithm
+
+- Implement any one of the Local Search techniques
